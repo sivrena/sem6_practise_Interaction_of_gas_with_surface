6e6035f2c4f807009e55e394cde492e387bee55b
sivrena <svinoroeva_m@mail.ru>
2024-02-22 07:56:07 +0300

    utility program

--- graph results.py
+++ graph results.py
@@ -14,10 +14,11 @@ for i in range (len(lines)):
     line = (lines[i]).split()
     isotherm_x[i] += float(line[0])
     isotherm_y[i] += float(line[1])
-    string = (line[4] + 'x' + line[5] + 'x' + line[6])
+    string = (line[3])
     plt.text(isotherm_x[i], isotherm_y[i], string)
 plt.grid(True)
 plt.plot(isotherm_x, isotherm_y, 'r^')
-lgnd = ['Temp. = ' + line[2] + 'K\n' + 'Part.num = ' + line[3]]
+lgnd = ['Temp. = ' + line[2] + 'K\n' + 'Part.num = ' + line[4] + 'x' + line[5] + 'x' + line[6] + 'nm\n'
+                                                                                'See graph for number of particles']
 plt.legend(lgnd, loc=4)
 plt.show()
